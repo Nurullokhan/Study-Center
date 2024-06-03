ea7e75ea2c6a038b03ea6aed0d8caaae4efd92bb
DostonbekAndroidDeveloper <husanovdostonbek2007@gmail.com>
2024-06-04 00:44:03 +0500

    Create

--- app/src/main/java/uz/zeeco/studycenter/HomeActivity.kt
+++ app/src/main/java/uz/zeeco/studycenter/HomeActivity.kt
@@ -1,33 +1,25 @@
 package uz.zeeco.studycenter
 
 import android.annotation.SuppressLint
-import android.content.ContentValues.TAG
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.messaging.FirebaseMessaging
+import uz.zeeco.studycenter.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityHomeBinding
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.home_main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -35,17 +27,17 @@ class HomeActivity : AppCompatActivity() {
         }
         // requestPermission()
         auth = FirebaseAuth.getInstance()
-        var currentUser = auth.currentUser
+        val currentUser = auth.currentUser
 
 //        Reference
-        val logout = findViewById<TextView>(R.id.idLogout)
+
 
         if (currentUser == null) {
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
 
-        logout.setOnClickListener {
+        binding.idLogout.setOnClickListener {
             auth.signOut()
             startActivity(Intent(this, MainActivity::class.java))
             finish()
@@ -53,44 +45,44 @@ class HomeActivity : AppCompatActivity() {
 
     }
 
-  /*  fun requestPermission() {
-        // This is only necessary for API level >= 33 (TIRAMISU)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            if (ContextCompat.checkSelfPermission(
-                    this,
-                    android.Manifest.permission.POST_NOTIFICATIONS
-                ) ==
-                PackageManager.PERMISSION_GRANTED
-            ) {
-                // FCM SDK (and your app) can post notifications.
-            } else if (shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
-                // TODO: display an educational UI explaining to the user the features that will be enabled
-                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
-                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
-                //       If the user selects "No thanks," allow the user to continue without notifications.
-            } else {
-                // Directly ask for the permission
-                requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
-            }
-        }
-    }
+    /*  fun requestPermission() {
+          // This is only necessary for API level >= 33 (TIRAMISU)
+          if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+              if (ContextCompat.checkSelfPermission(
+                      this,
+                      android.Manifest.permission.POST_NOTIFICATIONS
+                  ) ==
+                  PackageManager.PERMISSION_GRANTED
+              ) {
+                  // FCM SDK (and your app) can post notifications.
+              } else if (shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
+                  // TODO: display an educational UI explaining to the user the features that will be enabled
+                  //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
+                  //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+                  //       If the user selects "No thanks," allow the user to continue without notifications.
+              } else {
+                  // Directly ask for the permission
+                  requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+              }
+          }
+      }
 
-    private val requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission(),
-    ) { isGranted: Boolean ->
-        if (isGranted) {
-            FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
-                if (!task.isSuccessful) {
-                    Log.w(TAG, "Fetching FCM registration token failed", task.exception)
-                    return@OnCompleteListener
-                }
+      private val requestPermissionLauncher = registerForActivityResult(
+          ActivityResultContracts.RequestPermission(),
+      ) { isGranted: Boolean ->
+          if (isGranted) {
+              FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+                  if (!task.isSuccessful) {
+                      Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                      return@OnCompleteListener
+                  }
 
-                // Get new FCM registration token
-                val token = task.result
-                Log.w("FirebaseLogs", " Fetching FCM registration token:$token")
-                // Log and toast
+                  // Get new FCM registration token
+                  val token = task.result
+                  Log.w("FirebaseLogs", " Fetching FCM registration token:$token")
+                  // Log and toast
 
-            })
-            }
-        }*/
-    }
+              })
+              }
+          }*/
+}
